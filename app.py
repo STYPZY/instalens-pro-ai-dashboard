d5f58c4471cf1686c16b87884951026eb4e9fea7
STYPZY <vyshnavgovind28@gmail.com>
2026-03-17 12:01:36 +0530

    Updated code

--- app.py
+++ app.py
@@ -125,8 +125,7 @@ def analyze_snapchat_zip(job_id, zip_path):
 def get_upload_signature():
     timestamp = int(time.time())
     params = {
-        "timestamp": timestamp,
-        "resource_type": "raw",
+    "timestamp": timestamp,
     }
     signature = cloudinary.utils.api_sign_request(params, os.environ.get("CLOUDINARY_API_SECRET"))
     return jsonify({
@@ -468,4 +467,4 @@ def snapchat_export_chat_csv(dashboard_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
